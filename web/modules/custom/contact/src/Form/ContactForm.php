b4f3dad7d5ddcb4a7ffe5c53dd80b366ab79088f
Enitan Odupitan <odupitanenitan@gmail.com>
2023-10-09 12:45:05 +0300

    initial commit

---
+++ web/modules/custom/contact/src/Form/ContactForm.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\contact\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a contact form.
+ */
+final class ContactForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'contact_contact';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+
+    $form['guest_firstname'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('First Name'),
+      '#required' => TRUE,
+      '#maxlength' => 30,
+    ];
+
+    $form['guest_lastname'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Last Name'),
+      '#required' => TRUE,
+      '#maxlength' => 30,
+    ];
+
+    $form['guest_email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Guest Email'),
+      '#required' => TRUE,
+      '#maxlength' => 100,
+    ];
+    
+    $form['emp_zipcode'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Message'),
+      '#required' => TRUE,
+      '#maxlength' => 6,
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Send'),
+    ];
+
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    // @todo Validate the form here.
+    // Example:
+    // @code
+    //   if (mb_strlen($form_state->getValue('message')) < 10) {
+    //     $form_state->setErrorByName(
+    //       'message',
+    //       $this->t('Message should be at least 10 characters.'),
+    //     );
+    //   }
+    // @endcode
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $this->messenger()->addStatus($this->t('The message has been sent.'));
+    $form_state->setRedirect('<front>');
+  }
+
+}
